731c860a9196f8d714f8b30c136a3c7d931d547a
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-18 12:00:35 -0500

    got it all filled out, now to come up with a better theme and deploy it

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,50 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { TypingText, TitleText } from "../components";
+import { footerVariants } from "../utils/motion";
+import { socials } from "../constants";
 
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    intial="hidden"
+    whileInView="show"
+    className={`${styles.paddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex flex-wrap items-center justify-between gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">Enter the Metaverse</h4>
+        <button className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px] ">
+          <img src="/headset.svg" alt="headset" className="w-[24px] h-[24px] object-contain" />
+          <span className="font-normal text-white text-[16px]">Enter The Metaverse</span>
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex flex-wrap items-center justify-between gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
+          <p className="font-normal text-white opacity-50 text-[14px]">
+            Copyright 2021 - 2022 Metaversus. All rights reserved.
+          </p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                src={social.url}
+                alt={social.name}
+                key={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
