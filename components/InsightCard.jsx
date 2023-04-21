b4eb920656b78e7e2e474792dea02f9923896e14
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-20 22:10:23 -0500

    had to fix some breakpoint shenanigans, there are more though

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -3,28 +3,33 @@
 import { motion } from 'framer-motion';
 
 import { fadeIn, planetVariants } from '../utils/motion';
+import styles from '../styles';
 
 const InsightCard = ({ index, imgUrl, title, subtitle }) => (
-  <motion.div variants={fadeIn('up', 'spring', index * 0.5, 1)} className="flex flex-col gap-4 md:flex-row">
+  <motion.div variants={fadeIn('up', 'spring', index * 0.5, 1)} className="flex flex-col gap-4 md:flex-row sm:flex-row">
     <motion.div
       variants={fadeIn('right', 'spring', 0.7, 1)}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className="w-[450px] "
+      className="lg:w-[450px] md:w-[50px] sm:w-[45px]  "
     >
-      <img src={imgUrl} alt="vscode" className="md:w-[270px] h-full full rounded-b-3xl rounded-tl-3xl" />
+      <img
+        src={imgUrl}
+        alt="vscode"
+        className={`lg:w-[450px] md:w-[270px] sm:w-[70px]  h-full w-full rounded-b-3xl rounded-tl-3xl`}
+      />
     </motion.div>
     <div className="flex items-center justify-between w-full">
       <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
-        <h4 className="font-normal z-[2] lg:text-[42px] text-[26px] text-white ">{title}</h4>
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white z-10 ">{title}</h4>
         <p className="mt-[16px] font-normal z-[2] lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
       </div>
       <motion.div
         variants={planetVariants('left', 2)}
         initial="hidden"
         whileInView="show"
-        className="lg:flex hiden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
+        className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
       >
         <img src="arrow.svg" alt="arrow" className="w-[40%] h-[40%] object-contain " />
       </motion.div>
--- sections/VSCode.jsx
+++ sections/VSCode.jsx
@@ -1,31 +1,31 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { planetVariants, staggerContainer } from "../utils/motion";
-import { TypingText, TitleText, InsightCard } from "../components";
-import { insights } from "../constants";
+import styles from '../styles';
+import { planetVariants, staggerContainer } from '../utils/motion';
+import { TypingText, TitleText, InsightCard } from '../components';
+import { insights } from '../constants';
 
 const VSCode = () => (
   <section className={`${styles.paddings} relative z-10 p-10`}>
     <motion.img
       src="/VSCodeLogo.png"
-      variants={planetVariants(["up", "left"], 5)}
+      variants={planetVariants(['up', 'left'], 5)}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className="h-[400px] w-[400px] z-0 absolute right-[420px] top-[600px] opacity-70"
+      className="absolute lg:h-[400px] lg:w-[400px] lg:top-[750px] lg:right-[600px] h-[150px] w-[150px] -top-[15px] right-[130px] z-[0] opacity-50"
     ></motion.img>
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex  flex-col `}
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex  flex-col  `}
     >
-      <TypingText title="| VSCode" textStyles="text-center" />
-      <TitleText title="Visual Studio Code and IDE's" textStyles="text-center" />
+      <TypingText title="| VSCode" textStyles="text-center z-10" />
+      <TitleText title="Visual Studio Code and IDE's" textStyles="text-center z-10" />
       <div className="flex flex-col mt-[50px] gap-[30px]">
         {insights.map((insight, index) => (
           <InsightCard key={`insight-${index}`} {...insight} />
